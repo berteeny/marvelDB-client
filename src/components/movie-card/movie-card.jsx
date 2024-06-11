b775f25c889353fcf9238795a39b48a0f6d57ec9
berteeny <aegbertoni@gmail.com>
2024-06-11 13:52:01 -0700

    3.4

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+import PropTypes from "prop-types";
+
+//moie card function
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +11,26 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+//prop constraints defined here
+Moviecard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    release: PropTypes.string.isRequired,
+    length: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string.isRequired,
+      birth: PropTypes.Date.isRequired,
+      death: PropTypes.Date.isRequired,
+    }).isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    }).isRequired,
+    image: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
