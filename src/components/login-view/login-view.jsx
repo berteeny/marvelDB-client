0bda0cf45c889fd3a33ae4045d01ca0242d2ec4f
berteeny <aegbertoni@gmail.com>
2024-06-27 17:07:37 -0700

    Added error modals to signup and login views

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,10 +1,20 @@
 import { React, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const emptyForm = () => {
+    setUsername(""), setPassword("");
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -27,7 +37,8 @@ export const LoginView = ({ onLoggedIn }) => {
           localStorage.setItem("token", data.token);
           onLoggedIn(data.user, data.token);
         } else {
-          alert("No such user");
+          handleShow();
+          emptyForm();
         }
       })
       .catch((e) => {
@@ -36,30 +47,43 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <Form className="m-3" onSubmit={handleSubmit}>
-      <Form.Group controlId="loginUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="5"
-        />
-      </Form.Group>
-      <Form.Group className="mt-3" controlId="loginPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minLength="8"
-        />
-      </Form.Group>
-      <Button className="mt-3" variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Form className="m-3" onSubmit={handleSubmit}>
+        <Form.Group controlId="loginUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="5"
+          />
+        </Form.Group>
+        <Form.Group className="mt-3" controlId="loginPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="8"
+          />
+        </Form.Group>
+        <Button className="mt-3" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Your username or pssword is incorrect</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Okay
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 
 export const SignupView = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const emptyForm = () => {
+    setUsername(""), setPassword(""), setEmail(""), setBirthday("");
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -26,57 +35,69 @@ export const SignupView = () => {
       },
     }).then((response) => {
       if (response.ok) {
-        alert("Signup successful");
         window.location.assign("/login");
       } else {
-        alert("Signup failed");
+        handleShow();
+        emptyForm();
       }
     });
   };
 
   return (
-    <Form className="m-3" onSubmit={handleSubmit}>
-      <Form.Group controlId="signupUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="6"
-        />
-      </Form.Group>
-      <Form.Group className="mt-3" controlId="signupPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          minLength="8"
-        />
-      </Form.Group>
-      <Form.Group className="mt-3" controlId="signupEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Form.Group className="mt-3" controlId="signupBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date"
-          value={birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Button className="mt-3" variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Form className="m-3" onSubmit={handleSubmit}>
+        <Form.Group controlId="signupUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="6"
+          />
+        </Form.Group>
+        <Form.Group className="mt-3" controlId="signupPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="8"
+          />
+        </Form.Group>
+        <Form.Group className="mt-3" controlId="signupEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mt-3" controlId="signupBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button className="mt-3" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body className="my-3">
+          This username is unavailable. Please try another.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Okay
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
